9122201aa100ccb150fc88d69d8284fc3cdbbe26
erik.minasian <eminasian@webmd.net>
2024-12-17 12:26:09 +0400

    feat: clinet side pagination

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -1,22 +1,27 @@
-import Link from "next/link"
+'use client'
+
+import { useState } from "react"
 import ListItem from "./ListItem"
 import { PostType } from "../types"
 
-export default function List({ posts, page }: { posts: Array<PostType>, page: number}) {
+export default function List({ posts }: { posts: Array<PostType>}) {
 
+  const [currentpage, setPage] = useState(1)
   const pages = Math.floor(posts.length) + 1
-  const postsOfPage = posts.slice(9 * (page - 1), page * 9)
+  const postsOfPage = posts.slice(9 * (currentpage - 1), currentpage * 9)
+  
 
   return (
-    <div>
+    <div className="p-4">
       <div className="grid grid-cols-3">
       {
         postsOfPage.map(post => <ListItem key={post.id} post={post} />)
       }
       </div>
-      <div className="grid grid-cols-2">
-        {page > 1 && <Link href={`/posts?page=${page - 1}`}>Previous</Link>}
-        {page < pages && <Link href={`/posts?page=${page + 1}`}>Next</Link>}
+      <div className="flex justify-center p-4 gap-4">
+        {currentpage > 1 && <button onClick={() => { setPage(page => page - 1) }}>Previous</button>}
+        page {currentpage} of {pages}
+        {currentpage < pages && <button onClick={() => { setPage(page => page + 1) }}>Next</button>}
       </div>
     </div>
   )
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,20 +1,13 @@
 import List from "../components/List";
 
-export default async function PostsPage({ searchParams }: { searchParams: Promise<{ page: number }>}) {
+export default async function PostsPage() {
 
-  const { page } = await searchParams
-  const postsResponse = await fetch(
-    'http://localhost:3000/api/posts', 
-    { 
-      cache: 'force-cache',
-      next: { revalidate: 3600 }
-    }
-  )
+  const postsResponse = await fetch('http://localhost:3000/api/posts')
   const { posts } = await postsResponse.json()
 
   return (
     <div>
-      <List posts={posts} page={Number(page) ?? 1} />
+      <List posts={posts} />
     </div>
   );
 }
