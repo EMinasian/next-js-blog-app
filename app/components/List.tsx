f8fc6727b010665ad1394a10c6bef3df9a51c83e
erik.minasian <eminasian@webmd.net>
2024-12-18 10:24:39 +0400

    feat: posts per page

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -4,15 +4,29 @@ import { useState } from "react"
 import ListItem from "./ListItem"
 import { PostType } from "../types"
 
+const POSTS_PER_PAGE = [9, 12, 15, 18]
+
 export default function List({ posts }: { posts: Array<PostType>}) {
 
   const [currentpage, setPage] = useState(1)
-  const pages = Math.floor(posts.length) + 1
-  const postsOfPage = posts.slice(9 * (currentpage - 1), currentpage * 9)
-  
+  const [perPage, setPerPage] = useState(9)
+  const pages = Math.floor(posts.length / perPage) + 1
+  const postsOfPage = posts.slice(perPage * (currentpage - 1), currentpage * perPage)
 
+  function perPageButton(postsNum: number) {
+    return (
+      <button onClick={() => {setPerPage(postsNum)}}>{postsNum}</button>
+    )
+  } 
+  
   return (
     <div className="p-4">
+      <div className="flex gap-4 justify-center p-4">
+        Posts per page:
+        {
+          POSTS_PER_PAGE.map(num => perPageButton(num))
+        }
+      </div>
       <div className="grid grid-cols-3">
       {
         postsOfPage.map(post => <ListItem key={post.id} post={post} />)
--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -2,8 +2,9 @@ import { PostType } from "../types"
 
 export default function ListItem({ post }: { post: PostType}) {
   return (
-    <div>
+    <div className="p-2">
       <h2>{post.title}</h2>
+      <p>{post.summary}</p>
     </div>
   )
 }
