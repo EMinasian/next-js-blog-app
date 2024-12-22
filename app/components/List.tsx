93f458002cbc9b38a2aa8e7f0de2392affbd3f5f
erik.minasian <eminasian@webmd.net>
2024-12-22 18:58:31 +0400

    feat: search added

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 import ListItem from "./ListItem"
 import { PostType } from "../types"
 
@@ -8,16 +8,40 @@ const POSTS_PER_PAGE = [9, 12, 15, 18]
 
 export default function List({ posts }: { posts: Array<PostType>}) {
 
+  const searchRef = useRef<HTMLInputElement>(null)
+
   const [currentpage, setPage] = useState(1)
   const [perPage, setPerPage] = useState(9)
-  const pages = Math.floor(posts.length / perPage) + 1
-  const postsOfPage = posts.slice(perPage * (currentpage - 1), currentpage * perPage)
+  const [totalPosts, setPosts] = useState(posts)
+
+  const pages = Math.floor(totalPosts.length / perPage) + 1
+  const postsOfPage = totalPosts.slice(perPage * (currentpage - 1), currentpage * perPage)
 
   function perPageButton(postsNum: number) {
     return (
       <button onClick={() => {setPerPage(postsNum)}}>{postsNum}</button>
     )
   } 
+
+  function handleSearch() {
+
+    if (!searchRef.current?.value) {
+      return 
+    }
+
+    const reg = RegExp(searchRef.current?.value)
+    const searchedPosts = totalPosts.filter(post => {
+      const value = reg.test(post?.title)
+      return value
+    })
+    setPosts(searchedPosts)
+    setPage(1)
+  } 
+
+  function handleReset() {
+    setPosts(posts)
+    setPage(1)
+  }
   
   return (
     <div className="p-4">
@@ -26,6 +50,11 @@ export default function List({ posts }: { posts: Array<PostType>}) {
         {
           POSTS_PER_PAGE.map(num => perPageButton(num))
         }
+      </div>
+      <div className="flex gap-4 p-4">
+        <input name="search" ref={searchRef}/>
+        <button onClick={handleSearch}>Search</button>
+        <button onClick={handleReset}>Reset</button>
       </div>
       <div className="grid grid-cols-3">
       {
