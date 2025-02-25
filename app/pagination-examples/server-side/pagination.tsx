aa0fa5af77faeeb2a34dfe25be44f7af8c70cfd2
erik.minasian <eminasian@webmd.net>
2025-02-25 14:23:32 +0400

    fix: use transition

--- app/pagination-examples/server-side/pagination.tsx
+++ app/pagination-examples/server-side/pagination.tsx
@@ -1,15 +1,29 @@
 'use client'
 import { useRouter } from "next/navigation"
+import { useTransition } from "react"
 
 export default function Pagination({ pageCurrent, pages }: { pageCurrent: number, pages: number }) {
 
   const router = useRouter()
+  const [isPending, startTransition] = useTransition()
+
+  function prevPage() {
+    startTransition(() => {
+      router.push(`/pagination-examples/server-side?page=${pageCurrent - 1}`)
+    })
+  }
+
+  function nextPage() {
+    startTransition(() => {
+      router.push(`/pagination-examples/server-side?page=${pageCurrent + 1}`)
+    })
+  }
 
   return (
     <div className="flex justify-center p-4 gap-4">
-      {pageCurrent > 1 && <button onClick={() => { router.push(`/pagination-examples/server-side?page=${pageCurrent - 1}`) }}>Previous</button>}
+      {pageCurrent > 1 && <button onClick={prevPage}>Previous</button>}
       page {pageCurrent} of {pages}
-      {pageCurrent < pages && <button onClick={() => { router.push(`/pagination-examples/server-side?page=${pageCurrent + 1}`) }}>Next</button>}
+      {pageCurrent < pages && <button onClick={nextPage}>Next</button>}
     </div>
   )
 }
